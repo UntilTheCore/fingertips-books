88032f214c7796b6dacc26f00972eabef8305dcb
UntilTheCore <814898862@qq.com>
2020-07-24 09:45:58 +0800

    使用 v-model 指令代替对input标签value和input事件的绑定。

--- src/components/Money/Notes.vue
+++ src/components/Money/Notes.vue
@@ -2,15 +2,18 @@
     <div class="notes">
         <label>
             <span class="name">备注</span>
-            <input type="text" placeholder="在这里添加备注">
+            <input type="text" v-model="value" placeholder="在这里添加备注">
         </label>
     </div>
 </template>
 
 <script lang='ts'>
-    export default {
-        name: 'notes'
-    };
+    import Vue from 'vue';
+    import { Component } from 'vue-property-decorator';
+    @Component
+    export default class Notes extends Vue {
+        value = '';
+    }
 </script>
 
 <style lang='scss' scoped>
