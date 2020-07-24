f69874d7a5a96e831f35077bdb30b8f5b99fe6fc
UntilTheCore <814898862@qq.com>
2020-07-25 00:28:59 +0800

    完成<Labels />组件css布局。

--- src/views/Labels.vue
+++ src/views/Labels.vue
@@ -1,10 +1,27 @@
 <template>
-    <div class="labels">
-        <Layout>
-            <h1>Labels</h1>
-            <div class="tt">123</div>
-        </Layout>
-    </div>
+    <Layout class-prefix="label-bgc">
+        <div class="tags">
+            <ul>
+                <li>
+                    <span>衣</span>
+                    <Icon name="right"/>
+                </li>
+                <li>
+                    <span>食</span>
+                    <Icon name="right"/>
+                </li>
+                <li>
+                    <span>住</span>
+                    <Icon name="right"/>
+                </li>
+                <li>
+                    <span>行</span>
+                    <Icon name="right"/>
+                </li>
+            </ul>
+            <div class="newTag">新建标签</div>
+        </div>
+    </Layout>
 </template>
 
 <script lang='ts'>
@@ -13,8 +30,51 @@
     };
 </script>
 
+<style>
+    .label-bgc-content {
+        background: #f5f5f5;
+    }
+</style>
+
 <style lang='scss' scoped>
-    .tt {
-        background: #000;
+    .tags {
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        /*background: #f5f5f5;*/
+        /*height: calc(var(--vh, 1vh));*/
+        /*min-height: calc(var(--vh, 1vh));*/
+        width: 100%;
+        font-size: 16px;
+        ul {
+            width: inherit;
+            padding-left: 16px;
+            background-color: #fff;
+            li {
+                display: flex;
+                justify-content: space-between;
+                align-items: center;
+                height: 44px;
+                border-bottom: 1px solid #e6e6e6;
+                span {
+                }
+                svg {
+                    margin-right: 16px;
+                }
+            }
+        }
+
+        .newTag {
+            height: 40px;
+            width: 100px;
+            margin-top: 50px;
+            margin-bottom: 100px;
+            line-height: 40px;
+            text-align: center;
+            background: #767676;
+            border-radius: 4px;
+            color: #fff;
+
+        }
     }
 </style>
