012913be98f1f32891ef4e4a79135a36056794f0
UntilTheCore <814898862@qq.com>
2020-07-28 14:10:18 +0800

    使用dayjs处理日期格式

--- src/views/Statistics.vue
+++ src/views/Statistics.vue
@@ -7,8 +7,8 @@
             </template>
             <ul class="container">
                 <li v-for="(group,index) in resultByDay" :key="index">
-                    <h3 class="title">{{group.title}}</h3>
-                    <ul >
+                    <h3 class="title">{{beautifyTime(group.title)}}</h3>
+                    <ul>
                         <li v-for="(item,index) in group.items" :key="index" class="item">
                             <span>{{item.selectTags.name || '无'}}</span>
                             <span class="note">{{item.note}}</span>
@@ -41,15 +41,16 @@
     .container {
 
         padding : 0 12px;
+
         %style {
             height      : 40px;
             line-height : 40px;
         }
 
         ul {
-            border-radius: 15px;
-            padding : 0 10px;
-            background : #93ffce;
+            border-radius : 15px;
+            padding       : 0 10px;
+            background    : #fff;
         }
 
         .title {
@@ -57,14 +58,14 @@
         }
 
         li.item {
-            display : flex;
+            display         : flex;
             justify-content : space-between;
             @extend %style;
 
             .note {
                 margin-right : auto;
                 padding-left : 16px;
-                color : #999999;
+                color        : #999999;
             }
         }
 
@@ -75,6 +76,7 @@
     import Tabs from '@/components/Tabs.vue';
     import { Component } from 'vue-property-decorator';
     import Types from '@/components/Money/Types.vue';
+    import dayjs from 'dayjs';
 
     type hashTableValue = { title: string; items: RecordItem[] };
 
@@ -100,6 +102,25 @@
                 hashTable[title].items.push(recordList[i]);
             }
             return hashTable;
+        }
+
+        beautifyTime(str: string) {
+            const days = ['今天', '昨天', '前天'];
+            const api = dayjs(str);
+            const now = dayjs();
+            if ( api.isSame(now, 'day') ) {
+                return '今天';
+            } else if(api.isSame(now.subtract(1,'day'),'day')) {
+                return '昨天'
+            } else if( api.isSame(now.subtract(2,'day'),'day')) {
+                return '前天';
+            } else {
+                if(api.isSame(now,'year')) {
+                    return dayjs(str).format('M-D');
+                } else {
+                    return days(str).format('YYYY-M-D');
+                }
+            }
         }
 
         created() {
